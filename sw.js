eee9e6be5a59b9a1229dbc9f44810bcef945e2fb
juliobecerrad <159868500+juliobecerrad@users.noreply.github.com>
2025-12-02 12:04:39 -0500

    Update sw.js
    v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastos-app-v1';
+const CACHE_NAME = 'gastos-app-v2';
 const urlsToCache = [
   './',
   './controlgastos.html',
@@ -50,3 +50,4 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
