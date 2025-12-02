41727d44473312a971d73d6b173a1acf2fbf106a
juliobecerrad <159868500+juliobecerrad@users.noreply.github.com>
2025-12-02 11:46:51 -0500

    Update sw.js
    v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'control-gastos-cache-v25'; // ¡VERSIÓN ACTUALIZADA!
+const CACHE_NAME = 'control-gastos-cache-v26'; // ¡VERSIÓN ACTUALIZADA!
 // Lista de archivos base para que la app funcione offline.
 const urlsToCache = [
   './',
@@ -70,6 +70,7 @@ self.addEventListener('activate', (event) => {
   );
 
 });
+
 
 
 
